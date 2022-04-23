717e09e5e501f72fe653b8e469ee268cd49b9bb0
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-04-23 16:31:17 -0700

    run mfs2 and &if

---
+++ runmfs2if.sh
@@ -0,0 +1,13 @@
+dirname=$1
+if [ -z "$dirname" ]
+then
+    echo "specify project-dir"
+    exit 1
+fi
+i=0
+nfile=`ls ${dirname}/blif | wc -l`
+for i in `seq 0 $(($nfile - 1))`
+do
+    f="layer${i}.blif"
+    abc -c "read ${dirname}/blif/${f}; mfs2; &get; &if -K 6 -a; &put; write_blif ${dirname}/blifmap/${f}; write_verilog -fm ${dirname}/veropt/${f}.v"
+done
