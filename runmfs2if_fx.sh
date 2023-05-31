637461fe3e3540b1c96a9111e5e21b00e82fdde6
MyskYko <yoyuohlhjl@yahoo.co.jp>
2023-05-31 11:45:58 -0700

    fxed files

---
+++ runmfs2if_fx.sh
@@ -0,0 +1,5 @@
+f="dct_fx.blif"
+#abc -c "read_blif ${f}; mfs2; &get; &if -K 6 -a; &put; write_blif ${f}.abc.blif"
+abc -c "read_blif ${f}; &get; &if -K 6 -a; &put; write_blif ${f}.abc.blif"
+
+./postsynth.sh $f.abc.blif
